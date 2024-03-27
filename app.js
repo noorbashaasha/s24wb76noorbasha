3c8729e6f4f9f24d333e30c0d655ed8414e65529
noorbashaasha <noorbashaasha2017@gmail.com>
2024-03-27 17:53:00 -0500

    fords

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const fordRouter = require('./routes/ford');
+const fordsRouter = require('./routes/fords');
 const gridRouter = require('./routes/grid');
 
 var app = express();
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/ford', fordRouter);
+app.use('/ford', fordsRouter);
 app.use('/grid', gridRouter);
 
 
--- routes/fords.js
+++ routes/fords.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
  
-const ford = [
+const fords = [
   { name: 'Mustang', price: '2214000', year: 2021 },
   { name: ' Fusion', price: '1968000', year: 2022 },
   { name: 'Fiesta', price: '1312000', year: 2023 },
 ];
  
 router.get('/', (req, res) => {
-  res.render('ford', { title: 'Search Results', ford });
+  res.render('fords', { title: 'Search Results', fords });
 });
  
 module.exports = router;
