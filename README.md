f284b2154213385e6cd7b26271ead0557fa6f1de
noorbashaasha <noorbashaasha2017@gmail.com>
2024-03-27 17:39:15 -0500

    grid add

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [render link](https://s24wb76noorbasha.onrender.com)
 
-* ford_name(string)
-* ford_price(number)
-* ford_year(number)
+*ford_name(string)
+*ford_price(number)
+*ford_year(number)
--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const fordRouter = require('./routes/ford');
+const gridRouter = require('./routes/grid');
 
 var app = express();
 
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/ford', fordRouter);
+app.use('/grid', gridRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
